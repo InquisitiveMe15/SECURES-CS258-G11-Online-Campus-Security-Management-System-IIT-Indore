4c2b6e300e335deacb221c1d6d3c89e7a7274175
nehaj <nhjkhr.2021@gmail.com>
2022-03-18 00:36:07 +0530

    manage salary function working well

--- models/timeTable.js
+++ models/timeTable.js
@@ -1,25 +1,27 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 var timeTableSchema = new mongoose.Schema(
-    {
-        shift1_1 : String,
-        shift2_1 : String,
-        shift3_1 : String,
-        shift1_2 : String,
-        shift2_2 : String,
-        shift3_2 : String,
-        shift1_3 : String,
-        shift2_3 : String,
-        shift3_3 : String,
-        shift1_4 : String,
-        shift2_4 : String,
-        shift3_4 : String,
-        shift1_5 : String,
-        shift2_5 : String,
-        shift3_5 : String,
-        ID : String
+  {
+      from: Date,
+      to: Date,
+      ID: String,
+      shift1_1 : String,
+      shift2_1 : String,
+      shift3_1 : String,
+      shift1_2 : String,
+      shift2_2 : String,
+      shift3_2 : String,
+      shift1_3 : String,
+      shift2_3 : String,
+      shift3_3 : String,
+      shift1_4 : String,
+      shift2_4 : String,
+      shift3_4 : String,
+      shift1_5 : String,
+      shift2_5 : String,
+      shift3_5 : String
 
-    }
+  }
 );
 
 module.exports = mongoose.model("timeTable", timeTableSchema);
